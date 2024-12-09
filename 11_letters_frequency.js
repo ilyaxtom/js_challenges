3195a1a63dc3bdfc9b39b662ce1ca17872a8b996
ilyaxtom <xtom38@gmail.com>
2024-12-09 20:22:21 +0300

    13 challenges

---
+++ 11_letters_frequency.js
@@ -0,0 +1,31 @@
+/* 
+How often do the letters in your name repeat? 
+
+Write a function that counts how many times each letter of your name
+occurs. Your function should take in your first and last name and return
+an object where the keys are each character in your name, and the value
+is how many times that character appears in your name. 
+
+Example input: "Peggy Porth"
+Example output: {p: 2, e: 1, g: 2, y: 1, o: 1, r: 1, t: 1, h: 1}
+
+Your function should NOT count spaces and should not be case sensitive (a
+lowercase t and a capital T should be considered the same character).
+
+*/ 
+
+function countChars(str){
+   str = str.toLowerCase().split(' ').join('');
+   const frequency = {};
+   for (char of str) {
+        if (!frequency[char]) {
+            frequency[char] = 1;
+            continue;
+        }
+        frequency[char] += 1;
+   }
+
+   return frequency;
+}
+
+console.log(countChars("Peggy Porth"));
